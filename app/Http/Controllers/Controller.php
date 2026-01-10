f16a99ad4d9f2c32ca2650b8af8c2c89fbbdcd66
Bebe Kote <lifetroll6@gmail.com>
2026-01-10 23:29:02 +0200

    small inconsistency in abstract class controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-
 abstract class Controller
 {
     use AuthorizesRequests;
